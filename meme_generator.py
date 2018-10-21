a0504423b7a2e659bb5c6723b7d38e12111a2d3d
Aditya Rakhecha <dashingaditya.rakhecha@gmail.com>
2018-10-21 14:11:39 +0530

    3rd format added

--- meme_generator.py
+++ meme_generator.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 import argparse as arg
+import numpy as np
+import sys
+
 
 parser = arg.ArgumentParser('Meme Generator')
 
@@ -34,9 +37,10 @@ def meme_generator_1(image_path, top_text, font_path='impact/impact.ttf', font_s
         draw.text((x,y), line, fill='white', font=font)
         y += line_height
     img.save('meme-' + img.filename.split('/')[-1])
+    img.show()
 
 #format-2
-def meme_generator2(image_path, top_text, bottom_text, font_path='impact/impact.ttf', font_size=9):
+def meme_generator_2(image_path, top_text, bottom_text, font_path='impact/impact.ttf', font_size=9):
     
     
     img = Image.open(image_path)
@@ -72,18 +76,78 @@ def meme_generator2(image_path, top_text, bottom_text, font_path='impact/impact.
         draw.text((x,y), line, fill='white', font=font)
         y += line_height
 
-    
     img.save('meme-' + img.filename.split('/')[-1])
+
+#format-3
+def meme_generator_3(image1_path, image2_path, top_text, bottom_text, font_path='impact/impact.ttf', font_size=9):
+    
+    
+    img1 = Image.open(image1_path)
+    img2 = Image.open(image2_path)
+    images = map(Image.open, [image1_path, image2_path])
+ 
+    widths, heights = zip(*(i.size for i in images))
+
+    image_width = sum(widths)
+    image_height = max(heights)
+
+    img = Image.new('RGB', (image_width, image_height))
+
+    x_offset = 0
+    for im in images:
+    	img.paste(im, (x_offset, 0))
+    	x_offset += im.size[0]
+    
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font = font_path, size = int(image_height*font_size)//100)
+    
+    
+    top_text = top_text.upper()
+    bottom_text = bottom_text.upper()
+
+    
+    char_width, char_height = font.getsize('A')
+    chars_per_line = image_width // char_width
+    top_lines = textwrap.wrap(top_text, width = chars_per_line)
+    bottom_lines = textwrap.wrap(bottom_text, width = chars_per_line)
+    
+    
+    y = 10
+    for line in top_lines:
+        line_width, line_height = font.getsize(line)
+        x = (image_width - line_width)/2
+        draw.text((x,y), line, fill='white', font=font)
+        y += line_height
+
+   
+    y = image_height - char_height * len(bottom_lines) - 15
+    for line in bottom_lines:
+        line_width, line_height = font.getsize(line)
+        x = (image_width - line_width)/2
+        draw.text((x,y), line, fill='white', font=font)
+        y += line_height
+
+    
+    img.save('meme-' + img1.filename.split('/')[-1] + img2.filename.split('/')[-1] )
+
+    
 
 #main function
 if __name__ == '__main__':
 	if(args.format == "1"):
 		img = input("Enter the image path: ")
 		top_text = input("Input the top line here: ")
-		meme_generator1(img, top_text)
+		meme_generator_1(img, top_text)
 
 	if(args.format == "2"):
 		img = input("Enter the image path: ")
 		top_text = input("Input the top line here: ")
 		bottom_text = input("Input the bottom line here: ")
-		meme_generator2(img, top_text, bottom_text)
+		meme_generator_2(img, top_text, bottom_text)
+
+	if(args.format == "3"):
+		img1 = input("Enter image 1 path: ")
+		img2 = input("Enter image 2 path: ")
+		top_text = input("Input the top line here: ")
+		bottom_text = input("Input the bottom line here: ")
+		meme_generator_3(img1, img2, top_text, bottom_text)
