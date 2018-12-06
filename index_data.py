1ce896a2ebcfef9a4d32a7375c90bf6517438c86
Sukh Raj Limbu <limburaj65@gmail.com>
2018-12-07 02:51:26 +0530

    Update index_data.py
    Restructuring the code.

--- index_data.py
+++ index_data.py
@@ -5,12 +5,9 @@ data = []
 path = []
 description = []
 dirName = 'data'
-for subdir, dirs, files in os.walk(dirName):
-    
+for subdir, dirs, files in os.walk(dirName):    
     for file in files:
-        
         if file.endswith(".jpg"):
-
         	if os.path.exists(subdir + os.sep + file.split('.')[0] + '.json'): 
         		with open(subdir + os.sep + file.split('.')[0] + '.json') as f:
         			desc = json.load(f)
@@ -20,16 +17,15 @@ for subdir, dirs, files in os.walk(dirName):
         		data.append(file)
         		filepath = subdir + os.sep + file
         		path.append(filepath)
-
         		#print(file)
         	else:
         		description.append(subdir.split('\\')[-2])
         		data.append(file)
         		filepath = subdir + os.sep + file
         		path.append(filepath)
-        			
 
 with open('index.json', 'w') as f:
-	json.dump({'data' : [{"name": w , "description": x , "location" : y} for w,x,y in zip(data,description,path)]},f,sort_keys = False, indent = 4, 
-		ensure_ascii = False)
+	json.dump({'data' : [{"name": w , "description": x , "location" : y} 
+              for w, x, y in zip(data, description, path)]}, f, 
+              sort_keys = False, indent = 4, ensure_ascii = False)
         
