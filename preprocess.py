c4b717fadc4f44e95129b6bf8a5ad88521dd8d88
Sukh Raj Limbu <limburaj65@gmail.com>
2018-12-07 12:46:04 +0530

    Update preprocess.py
    Formatted the code

--- preprocess.py
+++ preprocess.py
@@ -1,28 +1,26 @@
 import os
-from PIL import Image 
+from PIL import Image
 import argparse as arg
 
 parser = arg.ArgumentParser()
-parser.add_argument('--data', type = str , help = "Enter image path", 
-                    default = "data")
-parser.add_argument('--width', type = int , help = "Enter width of image", 
-                    default = 600)
+parser.add_argument('--data', type=str, help='Enter image path',
+                    default='data')
+parser.add_argument('--width', type=int, help='Enter width of image',
+                    default=600)
 args = parser.parse_args()
 basewidth = args.width
 rootDir = args.data
 
-for dirName, subdirList, fileList in os.walk(rootDir):
-        for fname in fileList:
-                ext = os.path.splitext(fname)[-1].lower()
-                if ext.endswith(('.jpg','.png','.bmp')):
-                        img = Image.open(dirName + '/' + fname)
-                        wpercent = (basewidth / float(img.size[0]))
-                        hsize = int((float(img.size[1]) * float(wpercent)))
-                        img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-                        img.convert('RGB').save(dirName + '/' + 
-                                                os.path.splitext(fname)[0] + '.jpg')
-                        if ext.endswith(('.png','.bmp')):   #remove duplicates
-                            os.remove(dirName + '/' + fname)
-
-
-
+for (dirName, subdirList, fileList) in os.walk(rootDir):
+    for fname in fileList:
+        ext = os.path.splitext(fname)[-1].lower()
+        if ext.endswith(('.jpg', '.png', '.bmp')):
+            img = Image.open(dirName + '/' + fname)
+            wpercent = basewidth / float(img.size[0])
+            hsize = int(float(img.size[1]) * float(wpercent))
+            img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+            img.convert('RGB').save(dirName + '/'
+                                    + os.path.splitext(fname)[0]
+                                    + '.jpg')
+            if ext.endswith(('.png', '.bmp')):  # remove duplicates
+                os.remove(dirName + '/' + fname)
