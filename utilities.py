faa310211b992d4188613d5e1d71b1be6df5b159
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-01-05 00:26:19 +0530

    Updated utilities

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import ctypes # for windows
+import ctypes  # for windows
 from getpass import getpass
 from configparser import ConfigParser
 
@@ -15,6 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
+
 def set_desktop_background(img_path):
     platform = sys.platform
 
@@ -22,7 +23,7 @@ def set_desktop_background(img_path):
         '''
         Windows
         '''
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path , 0)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 0)
 
     elif platform == 'Darwin':
         '''
@@ -37,7 +38,8 @@ def set_desktop_background(img_path):
         sessionName = os.getenv("DESKTOP_SESSION")
 
         if sessionName == 'gnome':
-            os.system("gsettings set org.gnome.desktop.background picture-uri file:" + img_path)
+            os.system(
+                "gsettings set org.gnome.desktop.background picture-uri file:" + img_path)
 
         elif sessionName == 'kde':
             print('No support for KDE systems yet.')
@@ -48,80 +50,86 @@ def set_desktop_background(img_path):
         elif sessionName == 'i3':
             os.system("feh --bg-scale " + img_path)
 
+
 def upload_to_imgur(img_path):
-	config = ConfigParser()
-	config.read('auth.ini')
+    config = ConfigParser()
+    config.read('auth.ini')
 
-	client_id = config.get('imgur_credentials', 'client_id')
-	client_secret = config.get('imgur_credentials', 'client_secret')
+    client_id = config.get('imgur_credentials', 'client_id')
+    client_secret = config.get('imgur_credentials', 'client_secret')
 
-	client = ImgurClient(client_id, client_secret)
+    client = ImgurClient(client_id, client_secret)
 
-	user_res = input("Do you want to upload anonymously to imgur?(yes/no)")
+    user_res = input("Do you want to upload anonymously to imgur?(yes/no)")
 
-	if user_res.lower() == 'no':
-		try:
-			imgur_username = input("Enter username:")
-			imgur_password = getpass("Enter password:")
-			firefox_driver_path = os.getcwd() + os.sep + 'drivers' + os.sep + 'geckodriver'
+    if user_res.lower() == 'no':
+        try:
+            imgur_username = input("Enter username:")
+            imgur_password = getpass("Enter password:")
+            firefox_driver_path = os.getcwd() + os.sep + 'drivers' + os.sep + 'geckodriver'
 
-			authorization_url = client.get_auth_url('pin')
+            authorization_url = client.get_auth_url('pin')
 
-			driver = webdriver.Firefox(executable_path=firefox_driver_path)
-			driver.get(authorization_url)
+            driver = webdriver.Firefox(executable_path=firefox_driver_path)
+            driver.get(authorization_url)
 
-			username = driver.find_element_by_xpath('//*[@id="username"]')
-			password = driver.find_element_by_xpath('//*[@id="password"]')
+            username = driver.find_element_by_xpath('//*[@id="username"]')
+            password = driver.find_element_by_xpath('//*[@id="password"]')
 
-			username.clear()
-			password.clear()
+            username.clear()
+            password.clear()
 
-			username.send_keys(imgur_username)
-			password.send_keys(imgur_password)
+            username.send_keys(imgur_username)
+            password.send_keys(imgur_password)
 
-			driver.find_element_by_id("allow").click()
+            driver.find_element_by_id("allow").click()
 
-			timeout = 5
+            timeout = 5
 
-			try:
-				element_present = expected_conditions.presence_of_element_located((By.ID, 'pin'))
-				WebDriverWait(driver, timeout).until(element_present)
-				pin_element = driver.find_element_by_id('pin')
-				pin = pin_element.get_attribute("value")
+            try:
+                element_present = expected_conditions.presence_of_element_located(
+                    (By.ID, 'pin'))
+                WebDriverWait(driver, timeout).until(element_present)
+                pin_element = driver.find_element_by_id('pin')
+                pin = pin_element.get_attribute("value")
 
-			except TimeoutException as e:
-				print(e)
+            except TimeoutException as e:
+                print(e)
 
-			driver.close()
-			# print(pin)
-			credentials = client.authorize(pin, 'pin')
-			client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+            driver.close()
+            # print(pin)
+            credentials = client.authorize(pin, 'pin')
+            client.set_user_auth(
+                credentials['access_token'], credentials['refresh_token'])
 
-			config = {
-				'album': None,
-				'name': 'test name',
-				'title':  'test title',
-				'description': 'test description'
-			}
-			print("Uploading image...")
-			image = client.upload_from_path(img_path, config=config, anon=False)
-			print("Done! Check at", image['link'])
+            config = {
+                    'album': None,
+                    'name': 'test name',
+                    'title':  'test title',
+                    'description': 'test description'
+            }
+            print("Uploading image...")
+            image = client.upload_from_path(
+                img_path, config=config, anon=False)
+            print("Done! Check at", image['link'])
 
-		except Exception as e:
-			print(e)
+        except Exception as e:
+            print(e)
 
-	elif user_res.lower() == 'yes':
-		try:
-			response = client.upload_from_path(img_path)
+    elif user_res.lower() == 'yes':
+        try:
+            response = client.upload_from_path(img_path)
 
-		except Exception as e:
-			print(e)
+        except Exception as e:
+            print(e)
 
-		else:
-			print('Upload Successful! Check at', response['link'])
+        else:
+            print('Upload Successful! Check at', response['link'])
+
 
 def upload_to_twitter(img_path):
     pass
+
 
 if __name__ == '__main__':
     set_desktop_background(sys.argv[1])
