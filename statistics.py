bf8b0a694eb899bc403c56e877598eb8a478ec81
nickjiang2378 <nickjiang2378-1357924680@users.noreply.github.com>
2019-01-08 19:58:56 -0800

    Update statistics.py with pie chart

--- statistics.py
+++ statistics.py
@@ -19,59 +19,80 @@ for root, dirs, files in os.walk(args.data):
                 data_size += os.path.getsize(fpath)
                 if item.endswith(('.jpg', '.png', '.bmp', '.tif', '.gif')):
                         images += 1
-                
+                extension = "." + str(os.path.splitext(item)[1][1:].strip().lower())
                 #updates/creates the file sizes of the format of item
-                dotIndex = -1
-                for x in range(1,len(item)+1):
-                        if item[-x] == '.':
-                                dotIndex = len(item) - x
-                                break
-                if dotIndex >= 0 and item[dotIndex:] in fileSizes:
-                        fileSizes[item[dotIndex:]] += os.path.getsize(fpath)
-                elif dotIndex >= 0 and item[dotIndex:] not in fileSizes:
-                        fileSizes[item[dotIndex:]] = os.path.getsize(fpath)
-
+                if extension != ".":
+                        #print(extension)
+                        if extension in fileSizes:
+                                fileSizes[extension] += os.path.getsize(fpath)
+                        else:
+                                fileSizes[extension] = os.path.getsize(fpath)
         for item in dirs:
                 sub_dirs += 1
 
+#print(fileSizes)
 #creates pie chart for fileSizes
-colorSequence = ["white","red","blue","green","yellow","brown","orange"]
-total = sum(fileSizes.values())
-size = 2000
-image = Image.new("RGBA",(size,size),"#DDD")
-draw = ImageDraw.Draw(image,image.mode)
-currAngle = 0
-count = 0              #for moving through colorSequence
-font = ImageFont.truetype("calibri.ttf",size = 25)
-magnitude = size/4
+def pieChart(display):
+        colorSequence = ["white","red","blue","green","yellow","brown","orange"]
+        total = sum(fileSizes.values())
+        size = 2000
+        image = Image.new("RGBA",(size,size),"#DDD")
+        draw = ImageDraw.Draw(image,image.mode)
+        currAngle = 0
+        count = 0              #for moving through colorSequence
+        font = ImageFont.truetype("calibri.ttf",size = 25)
+        magnitude = size/4
+        
+        for formatName in fileSizes:
+                if fileSizes[formatName] != 0:
+                        newAngle = (fileSizes[formatName]/total)*360
+                        draw.pieslice((0,0,size,size),currAngle,currAngle + newAngle, fill = colorSequence[count % 7])
+                        currAngle += newAngle
+                        count += 1
 
-for formatName in fileSizes:
-    if fileSizes[formatName] != 0:
-        newAngle = (fileSizes[formatName]/total)*360
-        draw.pieslice((0,0,size,size),currAngle,currAngle + newAngle, fill = colorSequence[count % 7])
-        currAngle += newAngle
-        count += 1
+        currAngle = 0
+        #labels the slices
+        print()
+        for formatName in fileSizes:
+                if fileSizes[formatName] != 0:
+                        newAngle = (fileSizes[formatName]/total)*360
+                        xCoord = int(size/2 + magnitude*math.cos(math.radians((currAngle + (newAngle+currAngle))/2)))
+                        yCoord = int(size/2 + magnitude*math.sin(math.radians((currAngle + (newAngle+currAngle))/2)))
+                        #print(formatName, xCoord, yCoord)
+                        if "{:0.2f}".format(100*fileSizes[formatName]/total) != "0.00":
+                                draw.text((xCoord,yCoord), formatName + " : {:0.2f}%".format(100*fileSizes[formatName]/total),"black",font = font)
+                        if display:
+                                print(formatName + " : {:0.2f}%".format(100*fileSizes[formatName]/total))
+                        currAngle += newAngle
+                        magnitude += 50
+                        if magnitude > size/2:
+                                magnitude = size/4
 
-currAngle = 0
-#labels the slices
-for formatName in fileSizes:
-    if fileSizes[formatName] != 0:
-        newAngle = (fileSizes[formatName]/total)*360
-        xCoord = int(size/2 + magnitude*math.cos(math.radians((currAngle + (newAngle+currAngle))/2)))#-5 accounts for length of text
-        yCoord = int(size/2 + magnitude*math.sin(math.radians((currAngle + (newAngle+currAngle))/2)))
-        print(formatName, xCoord, yCoord)
-        if "{:0.2f}".format(100*fileSizes[formatName]/total) != "0.00":
-                draw.text((xCoord,yCoord), formatName + " : {:0.2f}%".format(100*fileSizes[formatName]/total),"black",font = font)
-        print(formatName + " : {:0.2f}%".format(100*fileSizes[formatName]/total))
-        currAngle += newAngle
-        magnitude += 50
-        if magnitude > size/2:
-            magnitude = size/4
+        del draw
+        image = image.resize((size//2,size//2))
+        image.save('pieChart.png','PNG')
+        if display:
+                image.show()
 
-del draw
-image = image.resize((size//2,size//2))
-image.show()
+print("Statistics\n----------")
+while True:
+        print("\n(1) Create and save pie chart with sizes of each file format")
+        print("(2) Create, save, and print a pie chart with sizes of each file format")
+        print("(3) Basic Statistical Data")
+        print("(4) Exit")
+        possibleChoices = ['1','2','3','4']
+        answer = input("Please enter a number from above: ")
+        while answer not in possibleChoices:
+                answer = input("Invalid. Please enter a number from above: ")
+        if answer == '1':
+                pieChart(False)
+        elif answer == '2':
+                pieChart(True)
+        elif answer == '3':
+                print()
+                print('Number of images:', images)
+                print('Total data size:', data_size, 'bytes')
+                print('Number of sub folders:', sub_dirs)
+        elif answer == '4':
+                break
 
-print('Number of images:', images)
-print('Total data size:', data_size, 'bytes')
-print('Number of sub folders:', sub_dirs)
