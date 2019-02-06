c5a30fc3b6af3169a1ac5971399da4537ad8dd1d
Aditya <adityaprakash.bobby@gmail.com>
2019-02-07 03:31:11 +0530

    Update meme_generator script

--- meme_generator.py
+++ meme_generator.py
@@ -6,8 +6,6 @@ import sys
 from formats.format1 import *
 from formats.format2 import *
 from formats.format3 import *
-from formats.format4 import *
-from formats.format5 import *
 from preprocess import preprocessImages
 import urllib.request
 import logo
@@ -62,41 +60,40 @@ if __name__ == '__main__':
                 print ('Empty or invalid arguments')
 
         if args.format == '1':
-            if args.image1 is not None and args.text1 is not None:
+            if args.text1 and args.text2 and args.image1:
                 preprocessImages(args.image1)
-                formatObj = Format1(args.image1, args.text1)
+                formatObj = Format1(image_path=args.image1,
+									top_text=args.text1,
+									bottom_text=args.text2)
+                formatObj.generate()
+    
+            elif args.text1 and args.image1:
+                preprocessImages(args.image1)
+                formatObj = Format1(image_path=args.image1,
+									top_text=args.text1,
+                                    bottom_text=None)
+                formatObj.generate()
+
+            elif args.text2 and args.image1:
+                preprocessImages(args.image1)
+                formatObj = Format1(image_path=args.image1,
+									top_text=None,
+									bottom_text=args.text2)
                 formatObj.generate()
             else:
-                print ('Missing arguments')
+                print('Missing arguments')
 
         if args.format == '2':
-            if args.image1 is not None and args.text1 is not None:
+            if args.image1 is not None and args.text1 is not None \
+                and args.image2 is not None and args.text2 is not None:
                 preprocessImages(args.image1)
-                formatObj = Format2(args.image1, args.text1)
+                preprocessImages(args.image2)
+                formatObj = Format2(args.image1, args.image2, args.text1, args.text2)
                 formatObj.generate()
             else:
                 print ('Missing arguments')
 
         if args.format == '3':
-            if args.image1 is not None and args.text1 is not None \
-                and args.text2 is not None:
-                preprocessImages(args.image1)
-                formatObj = Format3(args.image1, args.text1, args.text2)
-                formatObj.generate()
-            else:
-                print ('Missing arguments')
-
-        if args.format == '4':
-            if args.image1 is not None and args.text1 is not None \
-                and args.image2 is not None and args.text2 is not None:
-                preprocessImages(args.image1)
-                preprocessImages(args.image2)
-                formatObj = Format4(args.image1, args.image2, args.text1, args.text2)
-                formatObj.generate()
-            else:
-                print ('Missing arguments')
-
-        if args.format == '5':
             if args.image1 is not None and args.text1 is not None \
                     and args.image2 is not None and args.text2 is not None:
                 text_top = args.text1.split(',')
@@ -106,25 +103,11 @@ if __name__ == '__main__':
                 else:
                     preprocessImages(args.image1)
                     preprocessImages(args.image2)
-                    if text_top.__len__() == 2 and text_bottom.__len__() == 1:
-                        formatObj = Format5(image1_path=self.image1,
-                                     image2_path=self.image2,
-                                     text_individual1=text_top[0],
-                                     text_individual2=text_top[1],
-                                     top_text=None,
-                                     bottom_text=text_bottom[0])
-                        formatObj.generate()
-
-                    elif text_top.__len__() == 1 and text_bottom.__len__() == 2:
-                        formatObj = Format5(image1_path=args.image1,
-                                     image2_path=args.image2,
-                                     text_individual1=text_bottom[0],
-                                     text_individual2=text_bottom[1],
-                                     top_text=text_top[0],
-                                     bottom_text=None)
-                        formatObj.generate()
-                    else:
-                        print("Missing arguements")
+                    formatObj = Format3(image1_path=args.image1,
+										image2_path=args.image2,
+										top_text=text_top,
+										bottom_text=text_bottom)
+                    formatObj.generate()
             else:
                 print("Missing arguements")
 
@@ -141,65 +124,93 @@ if __name__ == '__main__':
 
         if format == '1':
             img = input('Enter the image path: ')
-            top_text = input('Input the top line here: ')
-            preprocessImages(img)
-            formatObj = Format1(img, top_text)
+            print(format1type1, format1type2, format1type3)
+            user_res = input('Select one of the formats (default : 1): ')
+            if user_res == '1':
+                preprocessImages(img)
+                top_text = input('Input the top line here: ')
+                formatObj = Format1(image_path=img,
+									top_text=top_text)
+            elif user_res == '2':
+                preprocessImages(img)
+                bottom_text = input('Input the bottom line here: ')
+                formatObj = Format1(image_path=img,
+									bottom_text=top_text)
+            elif user_res == '3':
+                preprocessImages(img)
+                top_text = input('Input the top line here: ')
+                bottom_text = input('Input the bottom line here: ')
+                formatObj = Format1(image_path=img,
+									top_text=top_text,
+									bottom_text=bottom_text)
             formatObj.generate()
 
         if format == '2':
-            img = input('Enter the image path: ')
+            img1 = input('Enter image 1 path: ')
+            img2 = input('Enter image 2 path: ')
+            top_text = input('Input the top line here: ')
             bottom_text = input('Input the bottom line here: ')
-            preprocessImages(img)
-            formatObj = Format2(img, bottom_text)
+            preprocessImages(img1)
+            preprocessImages(img2)
+            formatObj = Format2(img1, img2, top_text, bottom_text)
             formatObj.generate()
 
         if format == '3':
-            img = input('Enter the image path: ')
-            top_text = input('Input the top line here: ')
-            bottom_text = input('Input the bottom line here: ')
-            preprocessImages(img)
-            formatObj = Format3(img, top_text, bottom_text)
-            formatObj.generate()
-
-        if format == '4':
-            img1 = input('Enter image 1 path: ')
-            img2 = input('Enter image 2 path: ')
-            top_text = input('Input the top line here: ')
-            bottom_text = input('Input the bottom line here: ')
-            preprocessImages(img1)
-            preprocessImages(img2)
-            formatObj = Format4(img1, img2, top_text, bottom_text)
-            formatObj.generate()
-
-        if format == '5':
             img1 = input('Enter image 1 path: ')
             img2 = input('Enter image 2 path: ')
             preprocessImages(img1)
             preprocessImages(img2)
-            print(format5type1, format5type2)
+            print(format3type1, format3type2, format3type3, format3type4)
             type = input('Select the layout of meme (default : 1): ')
+
+            top_text = list()
+            bottom_text = list()
+
             if type == '1' or type == '':
-                top_text = input('Input the top spreading text: ')
-                text1 = input('Input the line for first image: ')
-                text2 = input('Input the line for second image: ')
-                formatObj = Format5(image1_path=img1,
-                             image2_path=img2,
-                             text_individual1=text1,
-                             text_individual2=text2,
-                             top_text=top_text,
-                             bottom_text=None)
-                formatObj.generate()
+                text1 = input('Input the top spreading text: ')
+                text2 = input('Input the bottom spreading text: ')
+                top_text.append(text1)
+                bottom_text.append(text2)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
             elif type == '2':
+                text1 = input('Input the top spreading text: ')
+                text2 = input('Input the line for first image: ')
+                text3 = input('Input the line for second image: ')
+                top_text.append(text1)
+                bottom_text.append(text2)
+                bottom_text.append(text3)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            elif type == '3':
                 text1 = input('Input the line for first image: ')
                 text2 = input('Input the line for second image: ')
-                bottom_text = input('Input the bottom spreading line: ')
-                formatObj = Format5(image1_path=img1,
-                             image2_path=img2,
-                             text_individual1=text1,
-                             text_individual2=text2,
-                             top_text=None,
-                             bottom_text=bottom_text)
-                formatObj.generate()
+                text3 = input('Input the bottom spreading line: ')
+                top_text.append(text1)
+                top_text.append(text2)
+                bottom_text.append(text3)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            elif type == '4':
+                text1 = input('Input the top line for first image: ')
+                text2 = input('Input the top line for second image: ')
+                text3 = input('Input the bottom line for first image: ')
+                text4 = input('Input the bottom line for second image: ')
+                top_text.append(text1)
+                top_text.append(text2)
+                bottom_text.append(text3)
+                bottom_text.append(text4)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            formatObj.generate()
 
     if args.mode == '2':
         if args.format is not None:
@@ -218,37 +229,28 @@ if __name__ == '__main__':
                 url = input('Enter image URL: ')
             download(url, 'meme_img')
             img = 'meme_img.jpg'
-            top_text = input('Input the top line here: ')
-            preprocessImages(img)
-            formatObj = Format1(img, top_text)
+            print(format1type1, format1type2, format1type3)
+            user_res = input('Select one of the formats (default : 1): ')
+            if user_res == '1' or user_res == '':
+                preprocessImages(img)
+                top_text = input('Input the top line here: ')
+                formatObj = Format1(image_path=img,
+									top_text=top_text)
+            elif user_res == '2':
+                preprocessImages(img)
+                bottom_text = input('Input the bottom line here: ')
+                formatObj = Format1(image_path=img,
+									bottom_text=top_text)
+            elif user_res == '3':
+                preprocessImages(img)
+                top_text = input('Input the top line here: ')
+                bottom_text = input('Input the bottom line here: ')
+                formatObj = Format1(image_path=img,
+									top_text=top_text,
+									bottom_text=bottom_text)
             formatObj.generate()
 
         if format == '2':
-            if args.url1 is not None:
-                url = args.url1
-            else:
-                url = input('Enter image URL: ')
-            download(url, 'meme_img')
-            img = 'meme_img.jpg'
-            bottom_text = input('Input the bottom line here: ')
-            preprocessImages(img)
-            formatObj = Format2(img, bottom_text)
-            formatObj.generate()
-
-        if format == '3':
-            if args.url1 is not None:
-                url = args.url1
-            else:
-                url = input('Enter image URL: ')
-            download(url, 'meme_img')
-            img = 'meme_img.jpg'
-            top_text = input('Input the top line here: ')
-            bottom_text = input('Input the bottom line here: ')
-            preprocessImages(img)
-            formatObj = Format3(img, top_text, bottom_text)
-            formatObj.generate()
-
-        if format == '4':
             if args.url1 is not None:
                 url1 = args.url1
             else:
@@ -265,10 +267,10 @@ if __name__ == '__main__':
             bottom_text = input('Input the bottom line here: ')
             preprocessImages(img1)
             preprocessImages(img2)
-            formatObj = Format4(img1, img2, top_text, bottom_text)
+            formatObj = Format2(img1, img2, top_text, bottom_text)
             formatObj.generate()
 
-        if format == '5':
+        if format == '3':
             if args.url1 is not None:
                 url1 = args.url1
             else:
@@ -282,28 +284,54 @@ if __name__ == '__main__':
             img1 = 'meme_img1.jpg'
             img2 = 'meme_img2.jpg'
 
-            print(format5type1, format5type2)
+            print(format3type1, format3type2, format3type3, format3type4)
             type = input('Select the layout of meme (default : 1): ')
 
+            top_text = list()
+            bottom_text = list()
+
             if type == '1' or type == '':
-                top_text = input('Input the top spreading text: ')
-                text1 = input('Input the line for first image: ')
-                text2 = input('Input the line for second image: ')
-                formatObj = Format5(image1_path=img1,
-                             image2_path=img2,
-                             text_individual1=text1,
-                             text_individual2=text2,
-                             top_text=top_text,
-                             bottom_text=None)
-                formatObj.generate()
+                text1 = input('Input the top spreading text: ')
+                text2 = input('Input the bottom spreading text: ')
+                top_text.append(text1)
+                bottom_text.append(text2)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
             elif type == '2':
+                text1 = input('Input the top spreading text: ')
+                text2 = input('Input the line for first image: ')
+                text3 = input('Input the line for second image: ')
+                top_text.append(text1)
+                bottom_text.append(text2)
+                bottom_text.append(text3)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            elif type == '3':
                 text1 = input('Input the line for first image: ')
                 text2 = input('Input the line for second image: ')
-                bottom_text = input('Input the bottom spreading line: ')
-                formatObj = Format5(image1_path=img1,
-                             image2_path=img2,
-                             text_individual1=text1,
-                             text_individual2=text2,
-                             top_text=None,
-                             bottom_text=bottom_text)
-                formatObj.generate()
+                text3 = input('Input the bottom spreading line: ')
+                top_text.append(text1)
+                top_text.append(text2)
+                bottom_text.append(text3)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            elif type == '4':
+                text1 = input('Input the top line for first image: ')
+                text2 = input('Input the top line for second image: ')
+                text3 = input('Input the bottom line for first image: ')
+                text4 = input('Input the bottom line for second image: ')
+                top_text.append(text1)
+                top_text.append(text2)
+                bottom_text.append(text3)
+                bottom_text.append(text4)
+                formatObj = Format3(image1_path=img1,
+									image2_path=img2,
+									top_text=top_text,
+									bottom_text=bottom_text)
+            formatObj.generate()
